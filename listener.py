15173e85389b3db30cc6c80c8bcb8b2c1b3e749c
tunahanahltc <123421987+tunahanahltc@users.noreply.github.com>
2025-03-24 00:58:05 +0300

    Update listener.py

--- listener.py
+++ listener.py
@@ -13,4 +13,4 @@ async def process_data(item: DataModel):
     return {"processed": processed_text}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10001)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
